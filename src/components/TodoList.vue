df1aa02ec44ded0492e066d37733e3004526f692
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2024-05-04 17:11:39 +0200

    feat: todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-2xl font-bold my-4">Todo List</h1>
+    <input v-model="newTodo" @keyup.enter="_addProject" type="text" class="border border-gray-400 rounded-lg p-2 w-full mb-4" placeholder="Add new todo...">
+    <ul>
+      <li v-for="({name, id}) in allProjects" :key="id" class="flex items-center justify-between bg-gray-100 p-2 mb-2">
+        <span v-if="!editingId || editingId !== id">{{ name }}</span>
+        <input v-else v-model="editedTodo" @keyup.enter="updateTodo" type="text" class="border border-gray-400 rounded-lg p-1 w-full" placeholder="Edit todo...">
+        <div>
+          <button @click="editTodo(id)" class="text-blue-500 hover:text-blue-700">{{ editingId === id ? 'Save' : 'Edit' }}</button>
+          <button @click="removeTodo(id)" class="text-red-500 hover:text-red-700">Remove</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { mapState, mapActions } from 'pinia'
+import { useProductStore } from '../stores/ProductStore'
+
+export default defineComponent({
+  name: 'TodoList',
+  data: () => {
+    return {
+      newTodo: '',
+      editingId: null,
+      editedTodo: null
+    }
+  },
+  computed: {
+    ...mapState(useProductStore, ['allProjects'])
+  },
+  methods: {
+    ...mapActions(useProductStore, ['getAllProjects', 'addProject', 'deleteProject', 'editProject']),
+    async _addProject(){
+      await this.addProject(this.newTodo)
+      this.newTodo = ''
+    },
+    async editTodo (_id: string) {
+      if (this.editingId === null) {
+       this.editingId = _id;
+        this.editedTodo = this.allProjects.find(({ id }) => id === _id)?.name;
+      } else {
+        await this.editProject({id: this.editingId, name: this.editedTodo})
+        this.editingId = null
+        this.editedTodo = null
+      }
+    },
+  },
+  mounted() {
+    this.getAllProjects()
+  }
+});
+</script>
+
+<style scoped>
+/* Add Tailwind CSS classes here */
+</style>
---
+++ src/stores/ProductStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+import { TodoistApi } from "@doist/todoist-api-typescript"
+
+const api = new TodoistApi("27791749084db6194209ec6e82c8eac685d6447a")
+
+
+export const useProductStore = defineStore('productStore', {
+  state: () => {
+    return {
+      productList: [],
+      allProjects: []
+    }
+  },
+  actions: {
+    getAllProjects() {
+      api.getProjects()
+    .then((projects: any) => this.allProjects = projects)
+    .catch((error: any) => console.log(error))
+    },
+    addProject(name: string) {
+      return api.addProject({ name })
+    .then((project: any) => this.allProjects = this.allProjects.concat(project))
+    .catch((error: any) => console.log(error))
+    },
+    deleteProject(_id: string) {
+      api.deleteProject(_id)
+    .then((isSuccess) => {
+      this.allProjects = this.allProjects.filter(({id}) => id !== _id)
+      console.log(isSuccess)}
+    )
+    .catch((error) => console.log(error))
+
+    },
+    editProject({id, name}) {
+      return api.updateProject(id, { name } )
+        .then(() => this.getAllProjects())
+        .catch((error) => console.log(error)) 
+    },
+  },
+})
